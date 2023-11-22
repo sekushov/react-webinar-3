d638f86744678f3705dc5c9e33312d39481853b8
Tim <sekushov@ya.ru>
2023-11-23 03:18:30 +0500

    3 task

--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ class Store {
   constructor(initState = {}) {
     this.state = initState;
     this.listeners = []; // Слушатели изменений состояния
+    this.id = this.state.list.length;
   }
 
   /**
@@ -42,9 +43,10 @@ class Store {
    * Добавление новой записи
    */
   addItem() {
+    this.id++;
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
+      list: [...this.state.list, {code: this.id, title: 'Новая запись'}]
     })
   };
 
@@ -59,6 +61,14 @@ class Store {
     })
   };
 
+  showSelectCount(item) {
+    if (!item.selectCount) {
+      item.selectCount = 0;
+      item.firstTitle = item.title;
+    }
+    item.selectCount++;
+    return `${item.firstTitle} | Выделяли ${item.selectCount} раз`
+  }
   /**
    * Выделение записи по коду
    * @param code
@@ -69,6 +79,11 @@ class Store {
       list: this.state.list.map(item => {
         if (item.code === code) {
           item.selected = !item.selected;
+          if (item.selected) {
+            item.title = this.showSelectCount(item);
+          }
+        } else {
+          item.selected = false;
         }
         return item;
       })
